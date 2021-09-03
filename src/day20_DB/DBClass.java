585a7a853fdc86058d9452c13420474eebea3f79
kimjinsu <soosos11234@gmail.com>
2021-09-03 13:59:26 +0900

    하는중

--- src/day20_DB/DBClass.java
+++ src/day20_DB/DBClass.java
@@ -2,7 +2,9 @@ package day20_DB;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 //db와 연동
 public class DBClass {
 	//url 객체 생성(오라클 정보,아이디, 비밀번호)
@@ -18,17 +20,67 @@ public class DBClass {
 			e.printStackTrace();
 		}
 	}
-	public void getList() {
-		//2. 데이터베이스 연결(con 은 DB에 연결된 객체다.)
+	public ArrayList<StudentDTO> getList() {
+		ArrayList<StudentDTO> list = new ArrayList<StudentDTO>();
+		
 		try {
-														//DB정보,아이디,비밀번호
+			//2. 데이터베이스 연결(con 은 DB에 연결된 객체다.)//DB정보,아이디,비밀번호
 			Connection con = DriverManager.getConnection(url, id, pwd);
-			System.out.println("연결이 잘 이뤄졌습니다 !");
-		} catch (SQLException e) {
+			System.out.println("연결이 잘  이뤄졌습니다 !");
+		
+		//3. 데이버테이스에 연결된 객체를 이용해서 명령어를 수행할 수 있는 객체를 얻어 온다.
+								//* == newst로부터 모든 데이터 가져오기
+			String sql = "select * from newst";
+			PreparedStatement ps = con.prepareStatement(sql);
+			
+			//4. 명령어를 수행할 수 있는 객체를 이용해서 명령어 수행
+			//5. 수행 결과를 저장한다.
+			ResultSet rs = ps.executeQuery();
+			// re = //bof//id n a // id n a // id n a // eof//로 구성됨
+			// rs.next이용시 한칸씩 앞으로 가며 출력함
+			while(rs.next()) {
+				//getList는 db관련만 다루기에 빼버림
+//				System.out.println("id : "+rs.getString("id"));
+//				System.out.println("name : "+rs.getString("name"));
+//				System.out.println("age : "+rs.getInt("age"));
+//				System.out.println("-----------------------------------");
+				StudentDTO dto = new StudentDTO();
+				dto.setId(rs.getString("id"));
+				dto.setName(rs.getString("name"));
+				dto.setAge(rs.getInt("age"));
+				list.add(dto);
+			}
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		//3. 데이버테이스에 연결된 객체를 이용해서 명령어를 수행할 수 있는 객체를 얻어 온다.
-		//4. 명령어를 수행할 수 있는 객체를 이용해서 명령어 수행
-		//5. 수행 결과를 저장한다.
+		return list;
+		}
+								//id가 중복돼서 오류, this로 교체
+	public StudentDTO searchST(String id ) {
+		//select * from newst where id = '222';
+		String sql = "select * from newst where id = '"+id+"'";
+				//dto = null 일시 데이터를 가져올 수 없음. 즉 찾고자하는 id 가 없다는 뜻임
+			//dto != null일시 찾고자하는 데이터가 있다. 즉 찾는 id 있음
+		StudentDTO dto = null;
+		try {
+			//1. 디비 연결
+			Connection con = DriverManager.getConnection(url,this.id,pwd);
+			System.out.println("-----연결 확인-----");
+			//2. 명령어(쿼리문)전송 객체 생성
+			PreparedStatement ps = con.prepareStatement(sql);
+			//3. 전송 후 결과값 저장
+			ResultSet rs = ps.executeQuery();
+			
+			if(rs.next()) {
+				
+				dto = new StudentDTO();
+				dto.setId(rs.getString("id"));
+				dto.setName(rs.getString("name"));
+				dto.setAge(rs.getInt("age"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return dto;
 	}
 }
--- src/day20_DB/MainClass.java
+++ src/day20_DB/MainClass.java
@@ -1,13 +1,14 @@
 package day20_DB;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MainClass {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		int num, age;
+		int num, age, result =0;
 		String id,name;
-		
+		ArrayList<StudentDTO> list = null;
 		DBClass db = new DBClass();
 		while(true) {
 			System.out.println("1.모든데이터 보기 2. 검색 3. 저장 4. 삭제 5. 수정");
@@ -15,11 +16,36 @@ public class MainClass {
 	 		switch(num) {
 			case 1:
 				//데이터베이스의 모든 데이터를 가져와서 보여준다.
-				db.getList();
+				list = db.getList();
+				for(int i =0; i<list.size(); i++) {
+					System.out.println("id : "+list.get(i).getId());
+					System.out.println("name : "+list.get(i).getName());
+					System.out.println("age : "+list.get(i).getAge());
+					System.out.println("-----------------------------------");
+				}
 				break;
-			case 2: break;
-			case 3: break;
-			case 4: break;
+			case 2: 
+			//검색 데이터를 데이터베이스에서 가져오기
+			System.out.println("검색 id 입력");
+			id = input.next();
+			StudentDTO dto = db.searchST(id);
+			if(dto != null) {
+			System.out.println("id : "+dto.getId());
+			System.out.println("name : "+dto.getName());
+			System.out.println("age : "+dto.getAge());
+			}else {
+				System.out.println("해당 아이디는 존재하지 않습니다.");
+			}
+			break;
+			case 3:
+				System.out.println("아이디 입력");
+				id = input.next();
+				System.out.println("이름 입력");
+				name = input.next();
+				System.out.println("나이 입력");
+				age = input.nextInt();
+				break;
+			case 4: break; 
 			case 5: break;
 			}
 		}
